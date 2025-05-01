92af07b528bed85353a99985a3675dcf6f18e445
najiibmohamed11 <abdinajibmohamed304@gmail.com>
2025-05-02 00:45:52 +0300

    adding production db uri

--- app/db/drizzle.ts
+++ app/db/drizzle.ts
@@ -3,5 +3,5 @@ if (!process.env.DATABASE_URI) {
   console.log("no env file");
 }
 export const db = drizzle(
-  "postgresql://postgres:12345678@localhost:5432/postgres"
+  process.env.DATABASE_URI||""
 );
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "drizzle-kit";
 
+if(!process.env.DATABASE_URI){
+  console.log("som thing went wron")
+}
+
 export default defineConfig({
   schema: "./app/db/schema.ts",
   out: "./migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: "postgresql://postgres:12345678@localhost:5432/postgres",
+    url: process.env.DATABASE_URI||"",
   },
 });
+
+//"postgresql://postgres:12345678@localhost:5432/postgres"
